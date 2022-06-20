4e93ee4e0155cf8877eb05a0268c5ed42094bb83
kiran-8999 <107863368+kiran-8999@users.noreply.github.com>
2022-06-20 19:15:28 +0530

    Add files via upload

---
+++ 30.c
@@ -0,0 +1,28 @@
+//=====================================================================
+//****** PROBLEMS ON N NUMBERS *******
+//===================================================
+//Array start
+//Array basics
+
+#include<stdio.h>
+int main()
+{
+    int Arr[5];
+    
+    printf("Enter Elements: \n");
+    scanf("%d",&Arr[0]);
+    scanf("%d",&Arr[1]);
+    scanf("%d",&Arr[2]);
+    scanf("%d",&Arr[3]);
+    scanf("%d",&Arr[4]);
+
+    printf("Elements You Entered : \n");
+
+    printf(" %d\n", Arr[0]);
+    printf(" %d\n", Arr[1]);
+    printf(" %d\n", Arr[2]);
+    printf(" %d\n", Arr[3]);
+    printf(" %d\n", Arr[4]);
+
+    return 0;
+}
